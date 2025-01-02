4648dde292315738f601bdda365c9c28cea51f14
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-01-02 17:51:37 +0530

    Done the Testing on Create a New Receipt when no receipts are created.

--- cypress/ui/e2e/step_definitions/newReceiptCreation.ts
+++ cypress/ui/e2e/step_definitions/newReceiptCreation.ts
@@ -1,6 +1,11 @@
-import { When } from "@badeball/cypress-cucumber-preprocessor";
+import { Then, When } from "@badeball/cypress-cucumber-preprocessor";
 import NewReceiptCreationPage from '../../pages/NewReceiptCreationPage';
 
-When("The user clicks on the Inventory Item",()=>{
-    NewReceiptCreationPage.clickInventoryItem();
+When("No Receipts are available",()=>{
+    NewReceiptCreationPage.checkReceiptsWindow();
 })
+
+Then("User should be able to create a new receipt",()=>{        
+    NewReceiptCreationPage.CreateNewReceipt();
+})
+
