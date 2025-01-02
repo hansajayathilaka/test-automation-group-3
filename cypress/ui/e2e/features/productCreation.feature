5892671e8a7a3440c21df889eb4faadef96edd08
karrlk <ravindu.karunaweera@ifs.com>
2025-01-02 21:44:28 +0530

    Created the Product Creation Flow

--- cypress/ui/e2e/features/productCreation.feature
+++ cypress/ui/e2e/features/productCreation.feature
@@ -1,6 +1,6 @@
 Feature: New Product Creation
 
-    Scenario: Create a New Product 
-        Given I am logged in
-        When No Product Exists in the Products Page
-        Then I should be able to Create a New Product
+  Scenario: Create a New Product
+    Given User logged in as an inventory admin
+    When User visits the Inventory Overview Page
+    Then User should be able to Create a New Product
--- cypress/ui/pages/ProductCreationPage.ts
+++ cypress/ui/pages/ProductCreationPage.ts
@@ -4,12 +4,20 @@ class ProductCreationPage {
     cy.visit("/odoo/inventory");
 
     // Verfications in the Inventory Overview Page
-    cy.get("article:first-of-type span.fw-bold.fs-4").should("have.text","Receipts").should("be.visible");
+    cy.get("article:first-of-type span.fw-bold.fs-4")
+      .should("have.text", "Receipts")
+      .should("be.visible");
     cy.log("Receipts Section is visible");
-    cy.get("body > div.o_action_manager > div > div.o_content > div > article:nth-child(2) > div.px-2 > a > span").should("have.text","Delivery Orders").should("be.visible");
+    cy.get(
+      "body > div.o_action_manager > div > div.o_content > div > article:nth-child(2) > div.px-2 > a > span"
+    )
+      .should("have.text", "Delivery Orders")
+      .should("be.visible");
     cy.log("Delivery Orders Section is visible");
-    cy.get("button.btn.btn-primary.oe_kanban_action[name='get_action_picking_tree_ready']").should("be.enabled");
-    cy.log('Open Button is enabled');
+    cy.get(
+      "button.btn.btn-primary.oe_kanban_action[name='get_action_picking_tree_ready']"
+    ).should("be.enabled");
+    cy.log("Open Button is enabled");
 
     // Open the Products Page
     cy.openMenu("Products", "stock.menu_stock_inventory_control");
@@ -19,7 +27,7 @@ class ProductCreationPage {
   createNewProduct() {
     // Verifications in the Empty Products Page
     cy.get("button.btn.btn-primary.o-kanban-button-new").should("be.enabled");
-    cy.log('New Btton is Enabled');
+    cy.log("New Btton is Enabled");
     cy.get(".o_view_nocontent p").then(($pTags) => {
       expect($pTags.eq(0)).to.contain.text(
         "No product found. Let's create one!"
@@ -28,22 +36,56 @@ class ProductCreationPage {
         "Track your stock quantities by creating storable products."
       );
     });
-    cy.log('Message Verified Successfully!');
+    cy.log("Message Verified Successfully!");
 
     // Create a New Product
-    cy.get("button.btn.btn-primary.o-kanban-button-new").should("be.enabled").click();
-    cy.get('#name_0').type('Laptop');
+    // Navigating to the Product Creation Page
+    cy.get("button.btn.btn-primary.o-kanban-button-new")
+      .should("be.enabled")
+      .click();
 
-    cy.get('#sale_ok_0').should('be.checked');
-    cy.log('Sales is Checked');
+    // Adding the General Information
+    cy.get("#name_0").type("Laptop");
+    cy.get("#sale_ok_0").should("be.checked");
+    cy.log("Sales is Checked");
+    cy.get("#purchase_ok_0").should("be.checked");
+    cy.log("Purchase is Checked");
+    cy.get(
+      "body > div.o_action_manager > div > div > div.o_content > div > div.o_form_sheet_bg > div.o_form_sheet.position-relative > div.o_notebook.d-flex.w-100.horizontal.flex-column > div.o_notebook_content.tab-content > div > div.o_group.row.align-items-start > div:nth-child(1) > div > div.o_cell.o_wrap_input.text-break > div > div > div:nth-child(1) > label"
+    ).click();
+    cy.log("Selected Goods as Product Type");
+    cy.get("#list_price_0").type("250000.00");
+    cy.log("Price Added Successfully");
+    cy.get("#standard_price_0").type("200000.00");
+    cy.log("Cost Added Successfully");
 
-    cy.get('#sale_ok_0').should('be.checked');
-    cy.log('Sales is Checked');
+    cy.get("#categ_id_0").click();
+    cy.get("#categ_id_0_0_0").click();
 
+    //Navigating to the Inventory Tab
+    cy.contains("Inventory")
+      .get(
+        "body > div.o_action_manager > div > div > div.o_content > div > div.o_form_sheet_bg > div.o_form_sheet.position-relative > div.o_notebook.d-flex.w-100.horizontal.flex-column > div.o_notebook_headers > ul > li:nth-child(2) > a"
+      )
+      .click();
 
+    //Adding the Inventory Details
+    cy.get("#responsible_id_0").click();
+    cy.get("#responsible_id_0_0_0").click();
+
+    cy.get("#weight_0").type("2.00");
+    cy.log("Weight Added Successfully");
+    cy.get("#volume_0").type("0.50");
+    cy.log("Volume Added Successfully");
+    cy.get("#sale_delay_0").type("7");
+    cy.log("Customer Lead Time Added Successfully");
+
+    //Navigating to the Products Window
+    cy.get(
+      "body > div.o_action_manager > div > div > div.o_control_panel.d-flex.flex-column.gap-3.px-3.pt-2.pb-3 > div > div.o_control_panel_breadcrumbs.d-flex.align-items-center.gap-1.order-0.h-lg-100 > div.o_breadcrumb.d-flex.flex-row.flex-md-column.align-self-stretch.justify-content-between.min-w-0 > ol > li > a"
+    ).click();
+    cy.log("Product Created Successfully");
   }
-
-
 }
 
 export default new ProductCreationPage();
