632a000dfbb19c39ec83b5e58f1cf8aed00f16e3
Hansa Jayathilaka <57087579+hansajayathilaka@users.noreply.github.com>
2025-01-02 23:24:35 +0530

    Update newReceiptCreation.ts

--- cypress/ui/e2e/step_definitions/newReceiptCreation.ts
+++ cypress/ui/e2e/step_definitions/newReceiptCreation.ts
@@ -1,15 +1,15 @@
 import { Then, When } from "@badeball/cypress-cucumber-preprocessor";
 import newReceiptCreationPage from '../../pages/NewReceiptCreationPage';
 
-When("User visits the Receipts create page",()=>{
+
+When("User visits the Receipts create page", () => {
     newReceiptCreationPage.visitNewReceiptCreationPage();
-})
+});
 
-When("User creates a new receipt with valid data",()=>{
+When("User creates a new receipt with valid data", () => {
     newReceiptCreationPage.createNewReceipt();
-})
+});
 
-Then("User should be able to see the receipt created successfully",()=>{        
+Then("User should be able to see the receipt created successfully", () => {        
     newReceiptCreationPage.checkReceiptCreation();
-})
-
+});
