1d8ebd0cb56b276effae6d513c8f8db8081fc917
UpekshaHerath <upekshadilshan000@gmail.com>
2025-01-04 03:22:42 +0530

    fix: problem with the import

--- cypress/ui/e2e/step_definitions/verifyDeliveryStatuses.ts
+++ cypress/ui/e2e/step_definitions/verifyDeliveryStatuses.ts
@@ -1,5 +1,5 @@
 import { Then, When } from "@badeball/cypress-cucumber-preprocessor";
-import VerifyDeliveryStatusesPage from "../../pages/verifyDeliveryStatusesPage";
+import VerifyDeliveryStatusesPage from "../../pages/VerifyDeliveryStatusesPage";
 
 When("User makes delivery as to do", () => {
     VerifyDeliveryStatusesPage.changeDraftDelieveryStatusToDo();
