1b3e2af63c372e20a8d9ba990a4c4f1cada19f52
Ravindu Karunaweera <94894276+RavinduLK@users.noreply.github.com>
2025-01-03 20:42:43 +0530

    create the product for receipt and changed the name of inventory over… (#48)
    Bugfixes

--- cypress/ui/e2e/step_definitions/productCreation.ts
+++ cypress/ui/e2e/step_definitions/productCreation.ts
@@ -1,11 +1,17 @@
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
 import ProductCreationPage from "../../pages/ProductCreationPage";
 
+Given("Create Product for receipt", () => {
+  ProductCreationPage.visit();
+  ProductCreationPage.createProduct("Laptop", "250000.00", "200000.00");
+})
+
 When("User visits the Product Creation Page", () => {
   ProductCreationPage.visit();
 });
 
 Then("User should be able to Create a New Product", () => {
-  ProductCreationPage.createNewProduct();
+  ProductCreationPage.createProduct("Laptop", "250000.00", "200000.00");
+  // ProductCreationPage.createNewProduct();
   ProductCreationPage.verifyNewProduct();
 });
